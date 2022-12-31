eff3271c43eaa487e9ae6a6a6ca5c8344b68b9dd
Lucas Furlan <furlan@hotmail.com.br>
2022-12-31 17:49:28 -0300

    fix checkbox stories

--- packages/docs/src/stories/Checkbox.stories.tsx
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -4,24 +4,16 @@ import { Box, Text, Checkbox, CheckboxProps } from '@devfurlan/react';
 export default {
   title: 'Form/Checkbox',
   component: Checkbox,
-  args: {
-    label: 'Accept terms of use',
-    'aria-label': 'Accept terms of use',
-    'aria-labelledby': 'accept-terms-of-use',
-  },
+  args: {},
   decorators: [
     (Story) => {
-      const storyContext = Story();
-
       return (
         <Box
           as="label"
           css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
         >
-          {storyContext}
-          <Text size="sm" id={storyContext.props['aria-labelledby']}>
-            {storyContext.props.label}
-          </Text>
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
         </Box>
       );
     },
